3e9e1159f0a17582dd60f9b91a0ef32ac8291fe8
kravisingh49 <142217280+kravisingh49@users.noreply.github.com>
2023-10-02 14:10:33 +0530

    Add files via upload
    The Program to Generate All Possible Arithmetic Progressions

---
+++ airthmatic using while.py
@@ -0,0 +1,21 @@
+##Find a sum of digits of given number
+
+n=int(input("enter digit you want to add"))
+sum=0
+
+while(n>0):
+    sum=(sum+n%10)
+    n=n//10
+print("sum of digits =",sum);
+
+##find product of given number
+n=int(int("Enter digit you want to multiply"));
+digit=1
+while(n>1):
+    digit=n%10
+    product*=digit
+    number//10
+print("product of digit:",product);
+       
+
+    
