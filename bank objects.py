802eac55bbff1d6353405eac713ed80967c53f4c
kravisingh49 <142217280+kravisingh49@users.noreply.github.com>
2023-10-09 14:48:38 +0530

    Update bank objects.py

--- bank objects.py
+++ bank objects.py
@@ -9,17 +9,23 @@ class bank:
         self.name = z
         print("Thankyou for creating an account with us,",self.name)
 
-    def withdraw(self):
-        pass
+    def withdraw(self, amt):
+        if self.account_balance >= amt:
+            self.acount_balance = self.account_balance - amt
+            print(f"Your account has been debited with {amt} dollars")
+            print(f"Your new account balance is {self.account_balance} dollars")
+        else:
+            print("you do not have enough balance")
 
-    def withdraw(self):
-        pass
 
-    def deposit(self):
-        pass
+    def deposit(self, amt):
+        self.account_balance = self.account_balance + amt
+        print(f"Your account has been credited with {amt} dollars")
+        print(f"Your new account balance is {self.account_balance} dollars")
 
     def check_balance(self):
-        pass
+        print("Welcome", self.name)
+        print(f"your account balance is {self.account_balance} dollars")
 
 objj= bank(245026, 500, "Ravi Kumar")
 objp= bank(245028, 1000, "Mradu Rathore")
