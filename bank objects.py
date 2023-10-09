f844217bbcd576aa1bd57b844dd6775c64aaf1bc
kravisingh49 <142217280+kravisingh49@users.noreply.github.com>
2023-10-09 14:34:53 +0530

    Add files via upload
    try to crete bank objects

---
+++ bank objects.py
@@ -0,0 +1,25 @@
+## blue print for creating bank objects.
+
+class bank:
+
+    def __init__(self, x, y, z):
+        self.account_number = x
+        self.initial_deposit = y
+        self.account_balance = y
+        self.name = z
+        print("Thankyou for creating an account with us,",self.name)
+
+    def withdraw(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def deposit(self):
+        pass
+
+    def check_balance(self):
+        pass
+
+objj= bank(245026, 500, "Ravi Kumar")
+objp= bank(245028, 1000, "Mradu Rathore")
